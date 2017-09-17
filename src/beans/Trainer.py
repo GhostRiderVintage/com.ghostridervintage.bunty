974b0a83857c7b3c6d01a9ebf6400ef49aab35c2
Mohommad Belal <mbelal@mobileiron.com>
2017-09-17 22:19:25 +0530

    testing

---
+++ src/beans/Trainer.py
@@ -0,0 +1,34 @@
+'''
+Created on Sep 17, 2017
+
+@author: mohommad_belal
+'''
+
+
+class Trainer(object):
+    '''
+    classdocs
+    '''
+    trainer_id = 0
+    __name ="boy"
+    __age =-1
+    __email = ""
+    __phone = ""
+    __gender =""
+    __dob = ""
+    __email = ""
+    __address = ""
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.__name = "Belal"
+        print(self.__name)
+
+
+trainer = Trainer()
+print(trainer.__name)
+
+
+
